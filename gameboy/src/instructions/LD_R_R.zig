e29afb34654d6f143fadea20f980e1ecd1b990eb
Jacob Enders <jacobenders1@gmail.com>
2026-02-22 16:44:06 -0500

    update

--- gameboy/src/instructions/LD_R_R.zig
+++
@@ -1,9 +0,0 @@
-const Registers = @import("../Registers.zig").Registers();
-
-pub fn LD_R_R(to: *u8, value: u8) void {
-    to.* = value;
-}
-
-pub fn LD_B_A(registers: *Registers) void {
-    registers.B = registers.A;
-}
--- gameboy/src/instructions/LD_R_aRR.zig
+++
@@ -1,7 +0,0 @@
-const Registers = @import("../Registers.zig").Registers();
-const Memory = @import("../Memory.zig").Memory();
-
-pub fn LD_A_aHLp(registers: *Registers, memory: *Memory) void {
-    registers.A = memory.read(registers.getHL());
-    registers.setHL(registers.getHL() + 1);
-}
