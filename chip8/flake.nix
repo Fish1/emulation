9dcd0091f611232343edda02cfb31f6d7fa9f518
Jacob Enders <jacobenders1@gmail.com>
2026-03-10 20:24:19 -0400

    init

--- chip8/flake.nix
+++ chip8/flake.nix
@@ -13,8 +13,45 @@
 		in {
 			devShells.default = pkgs.mkShell {
 				buildInputs = [
+					pkgs.pkg-config
 					pkgs.zig
 					pkgs.sdl3
+
+					pkgs.alsa-lib
+					pkgs.libjack2
+					pkgs.pipewire
+					pkgs.libpulseaudio
+						# pkgs.xorg.libX11
+					pkgs.xorg.libXcursor
+					pkgs.xorg.libXext
+					pkgs.xorg.libXi
+					pkgs.xorg.libXrandr
+					pkgs.xorg.libXScrnSaver
+					pkgs.xorg.libXxf86vm
+					pkgs.wayland
+					pkgs.wayland-protocols
+					pkgs.libxkbcommon
+					pkgs.libGL
+				];
+
+				LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+						# pkgs.sdl3
+
+						# pkgs.alsa-lib
+						# pkgs.libjack2
+						# pkgs.pipewire
+						# pkgs.libpulseaudio
+						pkgs.xorg.libX11
+						pkgs.xorg.libXcursor
+						pkgs.xorg.libXext
+						pkgs.xorg.libXi
+						pkgs.xorg.libXrandr
+						pkgs.xorg.libXScrnSaver
+						pkgs.xorg.libXxf86vm
+						pkgs.wayland
+						pkgs.wayland-protocols
+						pkgs.libxkbcommon
+						pkgs.libGL
 				];
 			};
 		}
